0c50232c2a1787ee412806e52a599660ea6d6c89
ehime <dodomeki@gmail.com>
2017-02-07 12:51:31 -0800

    Adding stat

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php // REQ: composer require aws/aws-sdk-php
+date_default_timezone_set('America/Los_Angeles');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+    USE Aws\CloudWatch\CloudWatchClient AS Client;
+
+$account  = str_replace("\n",'',shell_exec("aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text"));
+$location = dirname(__DIR__) . "/output/$account-describemetrics.csv";
+$profile  = shell_exec('echo $AWS_SECTION |xargs echo -n');
+
+$client = New Client([
+    'profile' => $profile,
+    'region'  => 'us-east-1',
+    'version' => 'latest',
+]);
+
+print_r($client->listMetrics([
+    'Namespace'  => 'System/Linux',
+    'MetricName' => 'MemoryUtilization',
+    'Dimensions' => [[
+        'Name'   => 'InstanceId',
+        'Value'  => 'i-1b4f819c',
+    ]]
+]));
+
