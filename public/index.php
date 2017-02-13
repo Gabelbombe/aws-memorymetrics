1660df56c90225b60bdb71a491b60447f012f14b
ehime <dodomeki@gmail.com>
2017-02-13 12:32:20 -0800

    Adding metrics

--- public/index.php
+++ public/index.php
@@ -9,18 +9,45 @@ $account  = str_replace("\n",'',shell_exec("aws ec2 describe-security-groups --g
 $location = dirname(__DIR__) . "/output/$account-describemetrics.csv";
 $profile  = shell_exec('echo $AWS_SECTION |xargs echo -n');
 
+    $start  = New DateTime;
+    $clone  = Clone $start;
+    $output = [];
+
+    $clone->modify('-2 month');
+    $csv = New CSVFile(dirname(__DIR__) . 'input/metrics.csv');
+
+    while ($clone != $start)
+    {
+        $output[$clone->format('m-d-Y')] = [];
+
+            $clone->modify('+1 day');
+    }
+
 $client = New Client([
     'profile' => $profile,
     'region'  => 'us-east-1',
     'version' => 'latest',
 ]);
 
-print_r($client->getMetricStatistics([
+$instanceId = 'i-1b4f819c';
+$util = $client->getMetricStatistics([
     'Namespace'  => 'System/Linux',
     'MetricName' => 'MemoryUtilization',
     'Dimensions' => [[
         'Name'   => 'InstanceId',
-        'Value'  => 'i-1b4f819c',
-    ]]
-]));
+        'Value'  => $instanceId,
+    ]],
 
+    'StartTime'  => $clone->modify('-2 month'),
+    'EndTime'    => $start,
+    'Period'     => 86400,         #seconds
+    'Statistics' => ['Average'],
+]);
+
+foreach ($util->get('Datapoints') AS $array)
+{
+    $output[$array['Timestamp']->format('m-d-Y')][$instanceId] = $array['Average'];
+}
+
+
+print_r($output);
---
+++ src/Model/CSVFile.php
@@ -0,0 +1,30 @@
+<?php
+
+
+Class CSVFile Extends SplFileObject
+{
+  private $keys;
+
+  public function __construct($file)
+  {
+    parent::__construct($file);
+    $this->setFlags(SplFileObject::READ_CSV);
+  }
+
+  public function rewind()
+  {
+    parent::rewind();
+    $this->keys = parent::current();
+    parent::next();
+  }
+
+  public function current()
+  {
+    return array_combine($this->keys, parent::current());
+  }
+
+  public function getKeys()
+  {
+    return $this->keys;
+  }
+}
